0fc15e585fa55c182943bbe9ae2f0006c1516124
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-03-14 12:27:09 -0400

    Gives chance to win half the emus you wagered

--- Prototypes/gambleprototype.py
+++ Prototypes/gambleprototype.py
@@ -13,18 +13,21 @@ import random
           msg = "You can't gamble that number of emus!"
           await client.send_message(message.channel, msg)
       else:
-          #chances of winning: 1 in 5
-          emucalnum = numemus * 2
           gamble = random.randint(0,9)
           #losing outcome
-          if gamble <= 7:
+          if gamble <= 4:
               user_add_value(message.author.id, -numemus, "emustorage")
               msg = "You lost, sorry... :("
               await client.send_message(message.channel, msg) 
-          #winning outcome
           else:
+              #big winning outcome
+              if gamble >=8:    
+                  emucalnum = numemus * 2
+              elif gamble > 4 and <= 7:
+                  emucalnum = numemus * 1.5
+                  emucalnum = int(emucalnum)
               fullemus = maxemus - get_value(message.author.id, "emudefense")
-              #if 
+              #if the number of emus goes over the limit
               if emucalnum > fullemus:
                   user_add_value(message.author.id, -get_value(message.author.id, "emustorage"), "emustorage")
                   user_add_value(message.author.id, fullemus, "emustorage")
@@ -34,4 +37,3 @@ import random
                   user_add_value(message.author.id, numemus, "emustorage")
                   msg = "You won! You gained " + str(numemus) + " emus!"
                   await client.send_message(message.channel, msg)
-          
