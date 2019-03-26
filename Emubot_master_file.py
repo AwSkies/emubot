bdb4722eb4d0153320e2023c0e396fc909b67762
Akash Shah <akash.w.shah@gmail.com>
2019-03-26 18:22:06 -0400

    Did some conceot testing for the gamble command and improved gamble command tocomprimise with Akiva's method.

--- Emubot_master_file.py
+++ Emubot_master_file.py
@@ -188,24 +188,10 @@ async def on_message(message):
             msg = 'You do not have permission to use this command.'
             await client.send_message(message.channel, msg)
 
-    #gives dummy bot the apropriate stats -----------------------------------------
-    if message.content.upper ().startswith('E!DUMMYSTATS'):
-        if "448272810561896448" in [role.id for role in message.author.roles]:
-            args = message.content.split(" ")
-            numcredits = args[1:2]
-            numemus = args[2:3]
-            numdefense = args[3:4]
-            user_add_value(559848326904938536, numcredits, 'credits')
-            user_add_value(559848326904938536, numemus, 'credits')
-            user_add_value(559848326904938536, numdefense, 'credits')
-            msg = "Dummy Bot has:"
-            msg += ":moneybag: `{}` credits.".format(get_value(559848326904938536, 'credits'))
-            msg += "\n<:emu:439821394700926976> `{}` emu(s) in storage.".format(get_value(559848326904938536, 'emustorage'))
-            msg += "\n:shield: `{}` emu(s) on defense.".format(get_value(559848326904938536, 'emudefense'))
-            await client.send_message(message.channel, msg)
-        else:
-            msg = 'You do not have permission to use this command!'
-            await client.send_message(message.channel, msg)
+    if message.content.upper () == "E!TEST":
+        args = message.content.split(" ")
+        print(args[0])
+        await client.send_message(message.channel, "you are all tested up!")
 
     #putting emus on defense command ----------------------------------------------
     if message.content.upper ().startswith('E!DEFEND') or message.content.upper ().startswith('E!DEFENCE') or message.content.upper ().startswith('E!DEFENSE'):
@@ -276,7 +262,7 @@ async def on_message(message):
         else:
             msg = '''You have no emus to take off defense! Remember, you can buy emus with e!buy and put them on defense with e!defend (number of emus you want to put on defense)!'''
             await client.send_message(message.channel, msg)
-
+    
     #attack command ---------------------------------------------------------------
     if message.content.upper ().startswith('E!ATTACK'):
         args = message.content.split(" ")
--- Prototypes/gambleprototype.py
+++ Prototypes/gambleprototype.py
@@ -1,66 +1,91 @@
+gamblesetup = dict()
+gamble_range_storage = dict()
+gamblecreds = dict()
 
-  #gamble command------------------------------------------------------------------
-  if message.content.upper ().startswith == "E!GAMBLE":
-      args = message.content.split
-      numemus = intify(args[1])
-      #checks if the number of emus gambled is less than or equal to zero
-      if numemus > get_value(message.author.id, 'emustorage'):
-          msg = "You don't have that number of emus to gamble!"
-          await client.send_message(message.channel, msg)
-        #checks is user has that many emus to gamble
-      elif numemus <= 0:
-          msg = "You can't gamble that number of emus!"
-          await client.send_message(message.channel, msg)
-      else:
-          def dicepicker():
-            dicenum = random.randint(1,6)
-            if dicenum == 1:
-                diceface = '<:dice1:559092222545625089>'
-            elif dicenum == 2:
-                diceface = '<:dice2:559092223044485140>'
-            elif dicenum == 3:
-                diceface = '<:dice3:559092223040552970>'
-            elif dicenum == 4:
-                diceface = '<:dice4:559092222935564301>'
-            elif dicenum == 5:
-                diceface = '<:dice5:559092223052873739>'
-            elif dicenum == 6:
-                diceface = '<:dice6:559092223145279491>'
-            return(diceface)
-          gamble = random.randint(0,9)
-          #losing outcome
-          if gamble <= 4:
-              outcome = 'lost'
-              user_add_value(message.author.id, -numemus, "emustorage")
-          else:
-              #big winning outcome
-              if gamble >= 8:
-                  emucalnum = numemus * 2
-              #medium winning outcome
-              elif gamble > 4 and <= 7:
-                  emucalnum = numemus * 1.5
-                  emucalnum = int(emucalnum)
-              fullemus = maxemus - get_value(message.author.id, "emudefense")
-              #if the number of emus goes over the limit
-              if emucalnum > fullemus:
-                  user_add_value(message.author.id, -get_value(message.author.id, "emustorage"), "emustorage")
-                  user_add_value(message.author.id, fullemus, "emustorage")
-              else:
-                  user_add_value(message.author.id, numemus, "emustorage")
-              outcome = 'won'
-          msg = 'Rolling the dice...' + dicepicker() + dicepicker()
-          edit = await client.send_message(message.channel, msg)
-          await asyncio.sleep(1)
-          edited = 'Rolling the dice...' + dicepicker() + dicepicker()
-          await client.edit_message(edit, edited)
-          await asyncio.sleep(1)
-          edited = 'Rolling the dice...' + dicepicker() + dicepicker()
-          await client.edit_message(edit, edited)
-          await asyncio.sleep(1)
-          #makes outcome
-          if outcome == 'lost': 
-              edited = 'You lost, sorry... :('
-              await client.edit_message(edit, edited) 
-          elif outcome == 'won':
-              edited = 'You won! you now have `{}` emus in storage!'.format(get_value(message.author.id, 'emustorage'))
-              await client.edit_message(edit, edited)
+    #gamble command----------------------------------------------------------------
+    if message.content.upper ().startswith == "E!GAMBLE":
+        args = message.content.split
+        numcreds = intify(args[1])
+        gamblerange = intify(args[2])
+        #checks if the number of credits gambled is less than or equal to zero
+        if numcreds > get_value(message.author.id, 'credits'):
+            msg = "You don't have that number of credits to gamble!"
+            await client.send_message(message.channel, msg)
+          #checks is user has that many credits to gamble
+        elif numcreds <= 0:
+            msg = "You can't gamble that number of credits!"
+            await client.send_message(message.channel, msg)
+        elif gamblerange < 1:
+            msg = "You can't pick from that amount of numbers!"
+            await client.send_message(message.channel, msg)
+        else:
+            msg = 'Pick a number between 1 and ' + str(gamblerange) + '''. If you wish to cancel, say "cancel".'''
+            await client.send_message(message.channel, msg)
+            gamblesetup[message.author.id] = True
+            gamble_range_storage[message.author] = gamblerange
+            gamblecreds[message.author.id] = numcreds
+
+    if gamblesetup[message.author.id] == True:
+        gamblerange = gamble_range_storage[message.author.id]
+        numcreds = gamblecreds[message.author.id]
+        args = message.content.split(" ")
+        guessnum = args[0]
+        try:
+            guessnum = int(guessnum)
+            if guessnum < 1:
+                msg = "You can't guess a number less than 1!"
+                await client.send_message(message.channel, msg)
+            elif guessnum > gamblerange:
+                msg = 'Pick a number between 1 and ' + str(gamblerange) + '''. If you wish to cancel, say "cancel".'''
+                await client.send_message(message.channel, msg)
+            else:
+                def dicepicker():
+                    dicenum = random.randint(1,6)
+                    if dicenum == 1:
+                        diceface = '<:dice1:559092222545625089>'
+                    elif dicenum == 2:
+                        diceface = '<:dice2:559092223044485140>'
+                    elif dicenum == 3:
+                        diceface = '<:dice3:559092223040552970>'
+                    elif dicenum == 4:
+                        diceface = '<:dice4:559092222935564301>'
+                    elif dicenum == 5:
+                        diceface = '<:dice5:559092223052873739>'
+                    elif dicenum == 6:
+                        diceface = '<:dice6:559092223145279491>'
+                    return(diceface)
+                gamble = random.randint(1,gamblerange)
+                #losing outcome
+                if not gamble == guessnum:
+                    outcome = 'lost'
+                    user_add_value(message.author.id, -numcreds, "credits")
+                else:
+                #winning outcome
+                    outcome = 'won'
+                    credcalnum = numcreds * int(gamblerange / 2)
+                    user_add_value(message.author.id, credcalnum, 'credits')
+                msg = 'Rolling the dice...' + dicepicker() + dicepicker()
+                edit = await client.send_message(message.channel, msg)
+                await asyncio.sleep(1)
+                edited = 'Rolling the dice...' + dicepicker() + dicepicker()
+                await client.edit_message(edit, edited)
+                await asyncio.sleep(1)
+                edited = 'Rolling the dice...' + dicepicker() + dicepicker()
+                await client.edit_message(edit, edited)
+                await asyncio.sleep(1)
+                #makes outcome
+                if outcome == 'lost': 
+                    edited = 'You lost, sorry... :('
+                    await client.edit_message(edit, edited) 
+                elif outcome == 'won':
+                    edited = 'You won! you gained ' + str(credcalnum) + ' credits!'
+                    await client.edit_message(edit, edited)
+        except ValueError:
+
+            elif message.content.upper () == "CANCEL":
+                msg = 'Canceled'
+                await client.send_message(message.author, msg)
+
+            else:
+                msg = 'Pick a number between 1 and ' + str(gamble_range_storage[message.author.id]) + '''. If you wish to cancel, say "cancel".'''
+                await client.send_message(message.channel, msg)
