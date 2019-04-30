9115a190a7c0694b98452d021f05b1b7e55bc6da
Akiva Ziskind <akiva.ziskind123@gmail.com>
2019-04-30 13:34:04 -0400

    added command that increases emu storage
    The problem is that I don't think I can just change it for one person.

--- Prototypes/addstorage.py
+++ Prototypes/addstorage.py
@@ -1,5 +1,17 @@
-    #give command------------------------------------------
+    #addstorage command------------------------------------------
     if message.content.upper ().startswith("E!ADDSTORAGE"):
-        args = message.content.split(" ")
-        numcreds = intify(args[1])
+        askedforaddstorage[message.author.id] = True
+        await client.send_message(message.channel, msg)
         
+    #saying no to adding storage
+    if (message.author.id in askedforaddstorage) and askedforaddstorage[message.author.id] and (message.content.upper () == 'NO'):
+        askedforaddstorage[message.author.id] = False
+        msg = "Canceled"
+        await client.send_message(message.channel,msg)
+        
+    #adding storage after saying yes
+    if (message.author.id in askedforaddstorage) and askedforaddstorage[message.author.id] and (message.content.upper () == 'YES'): 
+        askedforaddstorage[message.author.id] = False
+        ?
+        msg = "Emu storage increased by ?"
+        await client.send_message(message.channel,msg)
