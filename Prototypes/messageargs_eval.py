40203ddeca27933390260d0fd8c6388b9b023a8e
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-05-13 14:05:58 -0400

    Fixed messageargs_eval.py

--- Prototypes/messageargs_eval.py
+++ Prototypes/messageargs_eval.py
@@ -1,14 +1,13 @@
 import discord
 
-def messageargs_eval(content: str, message, targetargs: int, split = " "):
+def messageargs_eval(content: str, message: message, targetargs: int, split = " "):
     if not type(split) == str:
         raise TypeError("Inappropriate argument type.")
     else:
-        client = discord.Client()
-        args = len(self.content.split(" "))
-        if self.args < self.targetargs or self.args > self.targetargs:
+        args = len(message.content.split(split))
+        if not args < targetargs or args > targetargs:
             return True
         else:
             msg = "You are using the incorrect format for this command. Check the help commands for proper format."
-            await client.send_message(self.message.channel, msg)
+            await client.send_message(message.channel, msg)
             return False
