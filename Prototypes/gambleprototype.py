e6531da1a6758fbd3f9c9ffd2c87a80c70bad16b
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-06-06 09:12:13 -0400

    No, Akiva, we talked about this.
    No doing random literally useless syntax things the way you like them.

--- Prototypes/gambleprototype.py
+++ Prototypes/gambleprototype.py
@@ -87,10 +87,13 @@ gamblecreds = dict()
                     edited = 'You won! you gained ' + str(credcalnum) + ' credits!'
                     await client.edit_message(edit, edited)
                 gamblesetup[message.author.id] = False
+                
         except ValueError:
+            
             if message.content.upper () == "CANCEL":
                 msg = 'Canceled'
                 await client.send_message(message.author, msg)
+            
             else:
                 msg = 'Pick a number between 1 and ' + str(gamble_range_storage[message.author.id]) + '. If you wish to cancel, say "cancel".'
                 await client.send_message(message.channel, msg)
