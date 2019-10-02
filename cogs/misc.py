a1ebf4f4252733b7bfefe2fd3fa6e58de5b4db52
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-10-02 15:07:29 -0400

    Added changestatus command to misc cog
    Also edited the say and tsay commands to not repeat myself

--- cogs/misc.py
+++ cogs/misc.py
@@ -18,23 +18,45 @@ class Misc(masterclass):
     async def say(self, ctx, *args):
         if len(args) == 0:
             msg = "You can't make me send an empty message!"
-        elif len(args) == 1:
-            msg = ctx.author.mention + "says: \n" + args[1]
+            await ctx.send(msg)
         else:
-            msg = ctx.author.mention + "says: \n" + ' '.join(args)
-        await ctx.send(msg)
+            embed=discord.Embed()
+            if len(args) == 1:
+                embed.add_field(value = args[1], inline=False)
+            else:
+                embed.add_field(value = ' '.join(args), inline=False)
+            embed.set_footer(text = "-" + str(ctx.author))
+            await ctx.send(embed=embed)
         
-    @commands.command(name = "msay",
-                      aliases = ["sm"]
+    @commands.command(name = "testersay",
+                      aliases = ["tsay", "ts"]
                       hidden = True
 )
-    async def say(self, ctx, *args):
+    async def testersay(self, ctx, *args):
         if len(args) == 0:
             msg = "You can't make me send an empty message!"
         elif len(args) == 1:
             msg = args[1]
         else:
             msg = ' '.join(args)
+        await ctx.send(msg)
+    
+    @commands.command(name = "changestatus",
+                      aliases = ["cs"],
+                      hidden = True
+)
+    @commands.is_owner()
+    async def changegamestatus(self, ctx, *args):
+        if len(args) == 0:
+            msg = "You can't change the status to nothing!"
+        else:
+            if len(args) == 1:
+                msg = "Status changed to " + args[1]
+                game = discord.Game(name = args[1])
+            else:
+                msg = "States change to " + ' '.join(args)
+                game = discord.Game(name = ' '.join(args))
+            await bot.change_presence(game)
         await ctx.send(msg)
     
 def setup(bot):
