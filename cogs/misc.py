6fb199b4dc6ad59ac510d8ad4f48a4720fe65543
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-10-02 12:12:49 -0400

    Added say and msay commands to cog file

--- cogs/misc.py
+++ cogs/misc.py
@@ -8,14 +8,34 @@ class Misc(masterclass):
         super().__init()
         self.bot = bot
 
-    @commands.command(name = "",
-                      description = "",
-                      aliases = [""],
-                      brief = "",
-                      help = "",
-                      usage = ""
+    @commands.command(name = "say",
+                      description = "Makes the bot say whatever you say",
+                      aliases = ["s"],
+                      brief = "Makes the bot say whatever you say",
+                      help = "Makes the bot says whatever you put after the e!say",
+                      usage = "e!say [sentence] or e!say, makes the bot say [sentence]"
 )
-    async def (self, ctx, ):
+    async def say(self, ctx, *args):
+        if len(args) == 0:
+            msg = "You can't make me send an empty message!"
+        elif len(args) == 1:
+            msg = ctx.author.mention + "says: \n" + args[1]
+        else:
+            msg = ctx.author.mention + "says: \n" + ' '.join(args)
+        await ctx.send(msg)
+        
+    @commands.command(name = "msay",
+                      aliases = ["sm"]
+                      hidden = True
+)
+    async def say(self, ctx, *args):
+        if len(args) == 0:
+            msg = "You can't make me send an empty message!"
+        elif len(args) == 1:
+            msg = args[1]
+        else:
+            msg = ' '.join(args)
+        await ctx.send(msg)
     
 def setup(bot):
     bot.add_cog(Misc(bot))
