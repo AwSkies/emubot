f9d8a026bcc3dc598cd76881d6d31b139be68fbf
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-10-09 11:29:20 -0400

    Forgot to edit copied and pasted message

--- cogs/game.py
+++ cogs/game.py
@@ -69,7 +69,7 @@ class Game(masterclass):
         else:
             self.add_stats(ctx.author.id, numemus, 'defense')
             self.add_stats(ctx.author.id, -numemus, 'storage')
-            msg = '`{}` emu(s) added to defense. You can check you stats with e!stats'.format(numemus)
+            msg = '`{}` emu(s) taken off defense. You can check you stats with e!stats'.format(numemus)
         await ctx.send(msg)
         
     @commands.command(name = "",
