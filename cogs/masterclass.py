6f4c5314058d08232bdf23ac3fb8020e0913b597
Akiva Ziskind <akiva.ziskind123@gmail.com>
2019-10-23 12:14:43 -0400

    removed askedforaddstorage dict cuz Akash wanted me to for some reason

--- cogs/masterclass.py
+++ cogs/masterclass.py
@@ -11,7 +11,6 @@ class masterclass(object):
         self.ATTACKCOOLDOWN = 14400.0
         self.ALL_VALUE_TYPES = ['credits', 'storage', 'defense']
         self.ASKEDFORBUYEMU = dict()
-        self.ASKEDFORADDSTORAGE = dict()
         self.ASKEDFORRESET = dict()
         self.SPAMCATCH = dict()
         self.ATTACKTIMERCATCH = dict()
