3ced79f23b83961175cc008d56f82f1e15426c0d
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-10-28 07:48:06 -0400

    No Akiva, fillmeup isn't a prototype anymore

--- prototypes/fillmeupprototype.py
+++ prototypes/fillmeupprototype.py
@@ -14,25 +14,3 @@
         else:
             msg = 'You do not have permission to use this command!"
             await client.send_message(message.channel, msg)
-
-#fillmeup command rewrite
-    @commands.command(name = "fillmeup",
-                      description = "Gives credits to another user",
-                      aliases = ['fill', 'fmu'],
-                      brief = "Gives credits to another user",
-                      help = "???.",
-                      usage = "e!give [number] [@mention]",
-                      hidden = True
-)
-    async def give(self, ctx):
-        if "448272810561896448" in [role.id for role in message.author.roles]: #or something
-            self.add_stats(ctx.author.id, -(), 'credits')
-            self.add_stats(ctx.author.id, -(), 'storage')
-            self.add_stats(ctx.author.id, -(), 'defense')
-            self.add_stats(ctx.author.id, 20000, 'credits')
-            self.add_stats(ctx.author.id, (self.MAXEMUS - self.get_stats(ctx.author.id, 'defense')), 'storage')
-            self.add_stats(ctx.author.id, (self.MAXEMUS - self.get_stats(ctx.author.id, 'storage')), 'defense')
-            msg = 'You are topped off, tester!'
-        else:
-            msg = 'You do not have permission to use this command!'
-        ctx.send(msg)
