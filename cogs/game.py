3bbbf1c1edfff344daa147bedcac321d54198595
Akash Shah <akash.w.shah@gmail.com>
2019-11-07 18:51:24 -0500

    Not sure

--- cogs/game.py
+++ cogs/game.py
@@ -171,7 +171,6 @@ class Game(commands.Cog, Utils):
                 print('Message', ctx.message.content, 'caused exception:')
                 print(error)
                 print(type(error))
-                print('------')
             self.attack.reset_cooldown(ctx)
         else:
             msg = error
