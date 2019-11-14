04023fa4527633ad350598164f16ed483d70d477
Akash Shah <akash.w.shah@gmail.com>
2019-11-14 07:13:16 -0500

    Tried a fix, didn't work

--- cogs/game.py
+++ cogs/game.py
@@ -308,49 +308,50 @@ class Game(commands.Cog, Utils):
                     usage = 'guess [number]'
 )
     async def gambleguess(self, ctx, guessnum: int):
-        gamblerange = self.GAMBLEINFO[ctx.author.id]['range']
-        numcreds = self.GAMBLEINFO[ctx.author.id]['credits']
         if not self.GAMBLEINFO[ctx.author.id]['started'] or not ctx.author.id in self.GAMBLEINFO:
             msg = "You haven't made a gamble yet! Use e!gamble if you wish to."
             await ctx.send(msg)
-        elif guessnum < 1 or guessnum > gamblerange:
-            msg = 'Pick a number between **1** and **{}**. If you wish to cancel, use e!gamble cancel.'.format(gamblerange)
-            await ctx.send(msg)
         else:
-            num = random.randint(1, gamblerange)
-            def dicepicker():
-                def facepicker():
-                    dicenum = random.randint(1, 6)
-                    if dicenum == 1:
-                        diceface = '<:dice1:559092222545625089>'
-                    elif dicenum == 2:
-                        diceface = '<:dice2:559092223044485140>'
-                    elif dicenum == 3:
-                        diceface = '<:dice3:559092223040552970>'
-                    elif dicenum == 4:
-                        diceface = '<:dice4:559092222935564301>'
-                    elif dicenum == 5:
-                        diceface = '<:dice5:559092223052873739>'
-                    elif dicenum == 6:
-                        diceface = '<:dice6:559092223145279491>'
-                    return diceface
-                msg = 'Rolling the dice...' + facepicker() + facepicker()
-                return msg
-            msg = await ctx.send(dicepicker())
-            await asyncio.sleep(0.85)
-            for x in range(2):
-                await msg.edit(content = dicepicker())
-                await asyncio.sleep(0.85)
-            if not num == guessnum:
-                self.add_stats(ctx.author.id, -numcreds, "credits")
-                await msg.edit(content = 'Sorry, the number was `{}`. You lost `{}` credits... :('.format(num, numcreds))
+            gamblerange = self.GAMBLEINFO[ctx.author.id]['range']
+            numcreds = self.GAMBLEINFO[ctx.author.id]['credits']
+            if guessnum < 1 or guessnum > gamblerange:
+                msg = 'Pick a number between **1** and **{}**. If you wish to cancel, use e!gamble cancel.'.format(gamblerange)
+                await ctx.send(msg)
             else:
-                credcalnum = numcreds * int(gamblerange / 2)
-                self.add_stats(ctx.author.id, credcalnum, 'credits')
-                self.GAMBLEINFO[ctx.author.id]['started'] = False
-                self.GAMBLEINFO[ctx.author.id]['range']   = None
-                self.GAMBLEINFO[ctx.author.id]['credits'] = None
-                await msg.edit(content = ':confetti_ball: You won!!! You gained `{}` credits! :tada:'.format(credcalnum))
+                num = random.randint(1, gamblerange)
+                def dicepicker():
+                    def facepicker():
+                        dicenum = random.randint(1, 6)
+                        if dicenum == 1:
+                            diceface = '<:dice1:559092222545625089>'
+                        elif dicenum == 2:
+                            diceface = '<:dice2:559092223044485140>'
+                        elif dicenum == 3:
+                            diceface = '<:dice3:559092223040552970>'
+                        elif dicenum == 4:
+                            diceface = '<:dice4:559092222935564301>'
+                        elif dicenum == 5:
+                            diceface = '<:dice5:559092223052873739>'
+                        elif dicenum == 6:
+                            diceface = '<:dice6:559092223145279491>'
+                        return diceface
+                    msg = 'Rolling the dice...' + facepicker() + facepicker()
+                    return msg
+                msg = await ctx.send(dicepicker())
+                await asyncio.sleep(0.85)
+                for x in range(2):
+                    await msg.edit(content = dicepicker())
+                    await asyncio.sleep(0.85)
+                if not num == guessnum:
+                    self.add_stats(ctx.author.id, -numcreds, "credits")
+                    await msg.edit(content = 'Sorry, the number was `{}`. You lost `{}` credits... :('.format(num, numcreds))
+                else:
+                    credcalnum = numcreds * int(gamblerange / 2)
+                    self.add_stats(ctx.author.id, credcalnum, 'credits')
+                    self.GAMBLEINFO[ctx.author.id]['started'] = False
+                    self.GAMBLEINFO[ctx.author.id]['range']   = None
+                    self.GAMBLEINFO[ctx.author.id]['credits'] = None
+                    await msg.edit(content = ':confetti_ball: You won!!! You gained `{}` credits! :tada:'.format(credcalnum))
         
     @gamble.command(name = "cancel",
                     aliases = ['c', 'no', 'n'],
