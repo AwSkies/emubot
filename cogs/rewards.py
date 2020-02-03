afa178911c7cd38e6ce0993f0c286e574e6ea11b
Akiva Ziskind <akiva.ziskind123@gmail.com>
2020-02-03 10:18:09 -0500

    sell role command

--- cogs/rewards.py
+++ cogs/rewards.py
@@ -130,6 +130,37 @@ class Rewards(commands.Cog, Utils):
         except KeyError:
             msg = 'There are no rewards for this server!'
         await ctx.send(msg)
+
+    @rewards.command(
+        name = 'sell',
+        description = 'Sells a role that you bought for credits',
+        aliases = ['b'],
+        brief = 'Sells a role that you bought for credits',
+        help = 'Sell a registered role shown using the rewards command that you bought with credits',
+        usage = '[reward number]'
+)
+    @commands.bot_has_guild_permissions(manage_roles = True)
+    async def sell_rewards(self, ctx, r: str):
+        role = ctx.message.role_mentions[0]
+        with open('rewards.json', 'r') as f:
+            rewards = json.load(f)
+        try:
+            rewards = rewards[str(ctx.message.guild.id)]
+            reward = rewards[r - 1]
+            role = ctx.message.guild.get_role(int(rewards['role']))
+            if len(rewards) < r:
+                msg = "You're trying to sell a reward that isn't registered!"
+            elif not role in ctx.author.roles:
+                msg = "You don't have that role!"
+            elif ctx.message.guild.roles.index(ctx.author.roles[-1]) > ctx.message.guild.roles.index(ctx.message.guild.get_member(self.bot.id).roles[-1]):
+                msg = "I can't edit your roles, as you have a higher role than me. Please contact the moderators of the server and ask them to make my role higher so I can opertate to my full capacity."
+            else:
+                self.add_stats(ctx.author.id, reward['cost'], "credits")
+                ctx.author.remove_roles(role)
+                msg = "You sold the role {} for {} credits!".format(ctx.message.guild.get_role(int(reward['role'])).name, reward['cost'])
+        except KeyError:
+            msg = 'There are no rewards for this server!'
+        await ctx.send(msg)
     
 def setup(bot):
     bot.add_cog(Rewards(bot))
