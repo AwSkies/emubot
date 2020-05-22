eca0b218f24b953252f8579c9ef685d7faa2801e
Akash Shah <akash.w.shah@gmail.com>
2020-05-22 00:02:21 -0400

    Right command suggestions for incorrect commands by fuzzywuzzy
    Using the fuzzywuzzy string similarity package, after using a command that isn't found (so a command that doesn't exist) it will provide three commands that are closest to the one they used that they might have been trying to use.

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,13 +1,23 @@
 import discord
+import json
 
 from discord.ext import commands
 from discord.ext.commands import errors
 from cogs.UtilsLib import Utils
+from fuzzywuzzy import process
 
 class ErrorHandler(commands.Cog, Utils):
     def __init__(self, bot):
         Utils.__init__(self)
         self.bot = bot
+        self.all_commands = []
+        for command in [command for command in self.bot.commands if not command.hidden]:
+            name = command.name
+            self.all_commands.append(name)
+            if isinstance(command, commands.Group):
+                names = []
+                for subcommand in [subcommand for subcommand in command.commands if not subcommand.hidden]:
+                    self.all_commands.append('{} {}'.format(command.name, subcommand.name))
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
@@ -16,9 +26,6 @@ class ErrorHandler(commands.Cog, Utils):
             
         elif isinstance(error, errors.CheckFailure):
             return
-            
-        elif isinstance(error, errors.CommandNotFound):
-            msg = "That command doesn't exist!"
             
         elif isinstance(error, errors.MissingRequiredArgument):
             msg = "You are not using this command correctly. Use e!help command for information on how to use a a command. The square brackets [] around a word indicate a value you must provide after the command."
@@ -31,6 +38,12 @@ class ErrorHandler(commands.Cog, Utils):
 
         elif isinstance(error, errors.CommandOnCooldown):
             msg = error
+            
+        elif isinstance(error, errors.CommandNotFound):
+            corrections = []
+            for i in process.extract(ctx.message.content.lower().replace('e!', ''), self.all_commands, limit = 3):
+                corrections.append(i[0])
+            msg = "That command doesn't exist! \nDid you mean: `{}` | `{}` | `{}`".format(*corrections)
 
         else:
             msg = error
